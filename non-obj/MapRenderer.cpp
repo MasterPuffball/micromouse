4a3faa2e0bda1eaa94e8af11532dfad22badbf78
z5492338 <z5492338@ad.unsw.edu.au>
2025-08-13 14:01:09 +1000

    Try to implement the update wall for the display

--- non-obj/MapRenderer.cpp
+++ non-obj/MapRenderer.cpp
@@ -2,27 +2,58 @@
 
 MapRenderer::MapRenderer(U8G2 &display) : u8g2(display) {}
 
+
+void MapRenderer::setRobotPosition(uint8_t x, uint8_t y, uint8_t dir) {
+    robotX = x;
+    robotY = y;
+    robotDir = dir;
+}
+
 void MapRenderer::drawDot(uint8_t x, uint8_t y) {
-  u8g2.drawDisc(startX + x * dotSpacing, startY + y * dotSpacing, 0);
+    u8g2.drawDisc(startX + x * dotSpacing, startY + y * dotSpacing, 0);
 }
 
 void MapRenderer::drawGrid() {
-  for (uint8_t y = 0; y < gridSize; y++) {
-    for (uint8_t x = 0; x < gridSize; x++) {
-      drawDot(x, y);
+    for (uint8_t y = 0; y < gridSize; y++) {
+        for (uint8_t x = 0; x < gridSize; x++) {
+            drawDot(x, y);
+        }
     }
-  }
+}
+
+void MapRenderer::drawWalls() {
+    // Draw horizontal walls
+    for (uint8_t y = 0; y < gridSize; ++y) {
+        for (uint8_t x = 0; x < gridSize - 1; ++x) {
+            if (horizontalWalls[y][x]) {
+                u8g2.drawHLine(startX + x * dotSpacing, startY + y * dotSpacing, dotSpacing);
+            }
+        }
+    }
+    // Draw vertical walls
+    for (uint8_t y = 0; y < gridSize - 1; ++y) {
+        for (uint8_t x = 0; x < gridSize; ++x) {
+            if (verticalWalls[y][x]) {
+                u8g2.drawVLine(startX + x * dotSpacing, startY + y * dotSpacing, dotSpacing);
+            }
+        }
+    }
 }
 
 float MapRenderer::getCompletionPercentage() {
-  return ((visited_cells_count) / (MAZE_SIZE * MAZE_SIZE)) * 100.0f;
+    return ((visited_cells_count) / (MAZE_SIZE * MAZE_SIZE)) * 100.0f;
 }
 
 void MapRenderer::drawCompletion() {
-  u8g2.clearBuffer();
-  drawGrid();
-  u8g2.setFont(u8g2_font_6x10_tr);
-  u8g2.setCursor(75, 60);
-  u8g2.print(getCompletionPercentage(), 2);
-  u8g2.print("%");
+    u8g2.clearBuffer();
+    drawMap();
+    u8g2.setFont(u8g2_font_6x10_tr);
+    u8g2.setCursor(75, 60);
+    u8g2.print(getCompletionPercentage(), 2);
+    u8g2.print("%");
+}
+
+void MapRenderer::drawMap() {
+    drawGrid();
+    drawRobot();
 }
--- non-obj/MapRenderer.hpp
+++ non-obj/MapRenderer.hpp
@@ -8,6 +8,10 @@ public:
     void drawCompletion();
     float getCompletionPercentage();
 
+    void setRobotPosition(uint8_t x, uint8_t y, uint8_t dir);
+    void updateWalls(bool wallFront, bool wallLeft, bool wallRight);
+    void drawMap();
+
 private:
     U8G2 &u8g2;
     static constexpr uint8_t dotSpacing = 6;
@@ -19,6 +23,15 @@ private:
     // Track visited cells count
     uint8_t visited_cells_count = 0;
 
+    uint8_t robotX = 0;
+    uint8_t robotY = 0;
+    uint8_t robotDir = 0; // 0=up,1=right,2=down,3=left
+
+    // Wall storage
+    bool horizontalWalls[gridSize][gridSize] = {{false}};
+    bool verticalWalls[gridSize][gridSize] = {{false}};
+
     void drawDot(uint8_t x, uint8_t y);
+    void drawWalls();
 };
 
