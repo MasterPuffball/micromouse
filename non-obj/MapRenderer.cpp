f9a591c4150a283591f4bc2603fdd8a8d681523d
z5492338 <li_jiade@icloud.com>
2025-08-14 16:22:31 +1000

    Modified Mapping

--- non-obj/MapRenderer.cpp
+++ non-obj/MapRenderer.cpp
@@ -7,35 +7,29 @@ void MapRenderer::setRobotPosition(uint8_t x, uint8_t y, uint8_t dir) {
     robotX = x;
     robotY = y;
     robotDir = dir;
+    // Mark as visited
+    if (!visited[y][x]) {
+        visited[y][x] = true;
+        visited_cells_count++;
+    }
 }
 
 void MapRenderer::drawDot(uint8_t x, uint8_t y) {
     u8g2.drawDisc(startX + x * dotSpacing, startY + y * dotSpacing, 0);
 }
 
+void MapRenderer::drawHLine(uint8_t x, uint8_t y) {
+    u8g2.drawLine(x, y, x + dotSpacing, y);
+}
+
+void MapRenderer::drawVLine(uint8_t x, uint8_t y) {
+    u8g2.drawLine(x, y, x, y + dotSpacing);
+}
+
 void MapRenderer::drawGrid() {
     for (uint8_t y = 0; y < gridSize; y++) {
         for (uint8_t x = 0; x < gridSize; x++) {
             drawDot(x, y);
-        }
-    }
-}
-
-void MapRenderer::drawWalls() {
-    // Draw horizontal walls
-    for (uint8_t y = 0; y < gridSize; ++y) {
-        for (uint8_t x = 0; x < gridSize - 1; ++x) {
-            if (horizontalWalls[y][x]) {
-                u8g2.drawHLine(startX + x * dotSpacing, startY + y * dotSpacing, dotSpacing);
-            }
-        }
-    }
-    // Draw vertical walls
-    for (uint8_t y = 0; y < gridSize - 1; ++y) {
-        for (uint8_t x = 0; x < gridSize; ++x) {
-            if (verticalWalls[y][x]) {
-                u8g2.drawVLine(startX + x * dotSpacing, startY + y * dotSpacing, dotSpacing);
-            }
         }
     }
 }
@@ -55,5 +49,5 @@ void MapRenderer::drawCompletion() {
 
 void MapRenderer::drawMap() {
     drawGrid();
-    drawRobot();
+    //drawWalls();
 }
--- non-obj/MapRenderer.hpp
+++ non-obj/MapRenderer.hpp
@@ -21,17 +21,20 @@ private:
     static constexpr uint8_t MAZE_SIZE = 9;
 
     // Track visited cells count
+    bool visited[gridSize][gridSize] = {{false}};
     uint8_t visited_cells_count = 0;
 
     uint8_t robotX = 0;
     uint8_t robotY = 0;
     uint8_t robotDir = 0; // 0=up,1=right,2=down,3=left
 
-    // Wall storage
-    bool horizontalWalls[gridSize][gridSize] = {{false}};
-    bool verticalWalls[gridSize][gridSize] = {{false}};
+    uint8_t mazeWalls[gridSize][gridSize] = {{0}};
+
+    // CellWall mazeWalls[gridSize][gridSize];
+    void drawHLine(uint8_t x, uint8_t y);
+    void drawVLine(uint8_t x, uint8_t y);
+    void updateCellWall(uint8_t cx, uint8_t cy, char dir, bool isWall);
 
     void drawDot(uint8_t x, uint8_t y);
     void drawWalls();
 };
-
